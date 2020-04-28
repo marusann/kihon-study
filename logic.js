5d841f4a2919255ea2ce3dc64ccd5ed2f95eff2c
Yui Nakagawa <yui.le_reve.4ever@ezweb.ne.jp>
2020-04-28 08:58:06 +0000

    JSの練習

---
+++ logic.js
@@ -0,0 +1,12 @@
+(function(){
+    'use strict';
+    var age =16;
+    var result = null;
+    if(age >= 18){
+        result ='成年';
+    }else{
+        result ='未成年';
+    }
+    document.write(result);
+
+})();
